42d1a8a684f0a93375809765e941698e3f9ebc3b
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2022-07-02 16:10:41 +0700

    makes public

--- Sources/Autofixtures/Autofixtures.swift
+++ Sources/Autofixtures/Autofixtures.swift
@@ -8,7 +8,7 @@ public extension Autofixture {
     static var fix: Self { try! Self(from: FixtureDecoder()) }
 }
 
-extension Decodable {
+public extension Decodable {
     /// Make sure that property that you're setting is `var` and not `let` otherwise WritableKeyPath is not available
     func prop<Value>(_ kp: WritableKeyPath<Self, Value>, _ value: Value) -> Self {
         var copy = self
