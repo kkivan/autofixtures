74c17a50c0794b109d9f314771a98720c55d0f5b
Ivan Kvytakovskiy <ivan.kvyatkovski@gmail.com>
2022-07-18 21:32:02 +0700

    Improves docs

--- Sources/Autofixtures/Autofixtures.swift
+++ Sources/Autofixtures/Autofixtures.swift
@@ -6,20 +6,24 @@ import Foundation
 /// extension User: Override {
 ///     static let fixOverride = Self.fixDecoded.set(\.email, "email")
 /// }
-
 public protocol Override: Decodable {
+    /// Called internally instead of `var fix` when the type conforms to override
     static var fixOverride: Self { get }
 }
 
+/// Convinience conformance for CaseIterable Enums
+/// - returns first declared case
 public extension Override where Self: CaseIterable {
     static var fixOverride: Self { Self.allCases.first! }
 }
 
 public extension Decodable {
+    /// Use this method when you want to create an override for a type
     static var fixDecoded: Self {
         try! .init(from: FixtureDecoder())
     }
 
+    /// Prefer this method for creating fixtures
     static var fix: Self {
         guard let override = self as? Override.Type else {
             return .fixDecoded
@@ -29,6 +33,7 @@ public extension Decodable {
 }
 
 public extension Decodable {
+    /// Convinience setter, let you mutate one or more properties without passing other arguments
     /// Make sure that property that you're setting is `var` and not `let` otherwise WritableKeyPath is not available
     func set<Value>(_ kp: WritableKeyPath<Self, Value>, _ value: Value) -> Self {
         var copy = self
